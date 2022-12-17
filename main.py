2b5f47fa6624fceea9b435a5575ffbf86abdc12a
Sergey <serp2003.sf@gmail.com>
2022-12-17 12:58:17 +0300

    Change Welcom fo fans

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def main():
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
-    print('Воитель, Маг, Лекарь')
+    print('Воитель, Magic, Medic')
     char_class = choice_char_class()
     print(start_training(char_name, char_class))
 
